0811d1a712d457527b5ef001539da5c716dc83ce
Prasad Lungare <prasadlungare@gmail.com>
2019-09-04 22:58:07 +0530

    Update car_class.py
    Choose between 4 cars to drive and drive using 6 Gear options. Enjoy Driving!!

--- car_class.py
+++ car_class.py
@@ -1,3 +1,5 @@
+# Choose between 4 cars to drive and drive using 6 Gear options. Enjoy Driving!!
+
 class Car:
     
     def __init__(self,a):
@@ -67,7 +69,7 @@ class Car:
 
 
         
-print('Bienvenido! Choose any one cars listed below ')
+print('Bienvenido! Choose any one cars listed below to drive ')
 print('\n1.Maruti Alto\n2.Hyundai Accent\n3.Maruti Swift\n4.Mercedes Benz')
 
 a = int(input('\nEnter Choice : '))
